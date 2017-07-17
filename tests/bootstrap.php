6962ccf63a45ef6348aeb8eb5ea45a675c3cff55
Peter Gribanov <wcode404@gmail.com>
2017-07-17 19:20:24 +0300

    correct CS

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,7 @@
 <?php
 
-/**
- * AnimeDb package.
- *
- * @author    Peter Gribanov <info@peter-gribanov.ru>
- * @copyright Copyright (c) 2014, Peter Gribanov
- * @license   http://opensource.org/licenses/MIT
- */
-
 $file = __DIR__.'/../vendor/autoload.php';
+
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
 }
